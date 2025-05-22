8e9cfa03132e5eb0f6c9602ed2c8439839412f02
jpfisher72 <jpfisher72@gmail.com>
2025-05-22 13:02:18 -0400

    try again

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@
 import BrainSpatial from "./BrainSpatial"
 
 export default function Home() {
-  if (typeof window == 'undefined') return;
+  if (typeof document == 'undefined' || typeof window === 'undefined') return;
 
   return (
     <BrainSpatial gene="SP1" />
